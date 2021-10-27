7a5058cdf82b39933d2f9c056ed1c0380159951c
Tommy <tommy.poupineau@gmail.com>
2021-10-27 16:14:04 +0200

    GET2

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -24,6 +24,6 @@ module.exports = async function (context, req) {
     ];
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: tasks
     };
 }
